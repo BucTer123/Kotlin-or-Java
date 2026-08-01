fc123d683e4a1d579457d6ed8dacbf949205e127
SHALAWA <bambukvova3@gmail.com>
2026-08-01 13:37:26 +0200

    Add files via upload

---
+++ Mozaic(Hello_World)/main.kt
@@ -0,0 +1,8 @@
+import com.jakewharton.mosaic.runMosaic
+import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+
+fun main() = runMosaic {
+	Text("Hello, world!")
+}

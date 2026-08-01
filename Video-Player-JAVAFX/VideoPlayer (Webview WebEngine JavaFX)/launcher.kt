854098456b9affd9dbb829d2f511a304c686ef98
SHALAWA <bambukvova3@gmail.com>
2026-08-01 13:08:35 +0200

    Update and rename term.java to launcher.kt

---
+++ Video-Player-JAVAFX/VideoPlayer (Webview WebEngine JavaFX)/launcher.kt
@@ -0,0 +1,14 @@
+fun main() {
+    println("Welcome!\n")
+    println("Do you want to start VideoPlayherJava? (y/n) :")
+    String c = readLine()
+
+    if (c == "y" || c == "Y") {
+        println("Starting...")
+        classnameaudio.funcnameaudiotwo()
+        classnameaudio.funcnameaudio()
+    } else {
+        println("Bye!")
+        exit(0)
+    }
+}

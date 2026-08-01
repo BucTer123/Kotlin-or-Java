5aafcc8227025b0ef43793b4a92c31f3f5309b74
SHALAWA <bambukvova3@gmail.com>
2026-08-01 13:37:13 +0200

    Add files via upload

---
+++ Mozaic(Create_Button)/main.kt
@@ -0,0 +1,9 @@
+import com.jakewharton.mosaic.runMosaic
+import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+
+fun main() = runMosaic {
+	Text("Welcome!");
+	Box{ Text("[ OK! ]") }
+}

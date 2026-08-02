59f9c3f11d7c2e4e3f878334529cfb9c2e2de1b5
SHALAWA <bambukvova3@gmail.com>
2026-08-02 16:01:39 +0200

    Update out.kt

--- JKW/KW/out.kt
+++ JKW/KW/out.kt
@@ -1,4 +1,4 @@
-class kw {
+class kw_out {
 	fun output(String output_text) { println(output_text); }
 	fun outputln(String outputln_text) { println(outputln_text); }
 	fun echoln(String echoln_text) { println(echoln_text); }

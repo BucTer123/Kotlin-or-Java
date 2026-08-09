09bdd37ecd064bb8a28b25027cdc021e5ace5173
SHALAWA <bambukvova3@gmail.com>
2026-08-09 15:23:39 +0100

    Update out.java

--- JKW/JW/out.java
+++ JKW/JW/out.java
@@ -1,11 +1,5 @@
 public class jw_out {
-	 public static void echoln(String text_echoln) {
-	 	System.out.println(text_echoln)
-	 }
-	 public static void echo(String text_echo) {
-	 	System.out.println(text_echo)
-	 }
-	 public static void prints(String text_prints) {
-		 System.out.println(text_prints)
-	 }
+	 public static void echoln(String text_echoln) { System.out.println(text_echoln); }
+	 public static void echo(String text_echo) { System.out.println(text_echo); }
+	 public static void prints(String text_prints) { System.out.println(text_prints); }
 }

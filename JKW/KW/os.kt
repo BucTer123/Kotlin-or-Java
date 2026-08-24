a2f22568985b68d5420b551b19ed665478ff194c
Boba <bambukvova3@gmail.com>
2026-08-24 17:31:26 +0200

    Add systemd function to execute commands

--- JKW/KW/os.kt
+++ JKW/KW/os.kt
@@ -3,4 +3,5 @@ class kw_os {
 	fun ret(Boolean return_status) { return return_status }
 	fun create_directory(String name_directory_create) { File.mkdir(name_directory_create); }
 	fun remove_directory(String name_directory_remove) { File.rmdir(name_directory_remove); }
+	fun systemd(String name_command) { Runtime.getRuntime().exec(name_command); }
 }

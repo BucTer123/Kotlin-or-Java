58cc456feea13dc1785a6acc3713b47feec6ef47
Boba <bambukvova3@gmail.com>
2026-08-24 17:29:50 +0200

    Update os.java

--- JKW/JW/os.java
+++ JKW/JW/os.java
@@ -5,5 +5,6 @@ public class jw_sys throws IOExpection {
 	public static void create_directory(String name_directory_create) { File dir = new File(name_directory_create); }
 	public static void remove_directory(String name_directory_remove) { Files.delete(name_directory_remove); }
 	public static void ext(int status) { exit(status); }
-	public static void ret(boolean boolean_return) { return boolean_return; }
+	public static void ret(boolean boolean_return) { return boolean_return; }\
+	public static void systemd(String name_command) { Runtime.getRuntime(name_command).exec(); }
 }

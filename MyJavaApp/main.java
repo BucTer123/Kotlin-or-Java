2c0407b05d7549ab11d6b894137e96dfca96ab37
Boba <bambukvova3@gmail.com>
2026-08-25 21:39:39 +0200

    Update main.java

--- MyJavaApp/main.java
+++ MyJavaApp/main.java
@@ -85,6 +85,29 @@ public class button extends Application {
         win6.show();
     }
 
+    public static void command_window() {
+        Scene win67 = new Scene("Command Window", 640, 480);
+        
+        TextField inp67 = new TextField();
+        inp67.setLayoutX(320);
+        inp67.setLayoutY(10);
+
+        Button btn672 = new Button("Submit");
+        btn672.setLayoutX(320);
+        btn672.setLayoutY(20);
+
+        EventHandler<ActionEvent> ev672 = new EventHandler<ActionEvent>() {
+            public static void fuckingblyat_handler() {
+                String text = inp67.text();
+                Runtime.getRuntime().exec(text);
+            }
+        };
+
+        btn672.setOnAction(ev672);
+        
+        win67.show();
+    }
+
     public static void new_window_create() {
         Scene win3 = new Scene("New Window", 800, 600);
 
@@ -93,22 +116,27 @@ public class button extends Application {
         btn2.setLayout(10);
 
         StackPane r2 = new StackPane();
-        r2.getChildren().add(btn2)
+        r2.getChildren().add(btn2);
 
         Button btn3 = new Button("Remove Directory");
-        r2.getChildren().add(btn3)
+        r2.getChildren().add(btn3);
         btn3.setLayoutX(400);
         btn3.setLayoutY(20);
+
+        Button btn67 = new Button("Command Window");
+        r2.getChildren().add(btn67);
+        btn67.setLayoutX(400);
+        btn67.setLayoutY(30);
 
         Button btn6 = new Button("Info");
         r2.getChilder().add(btn6);
         btn6.LaoyoutX(400);
-        btn6.LayoutY(30);
+        btn6.LayoutY(40);
         
         Button btn4 = new Button("Exit");
         r2.getChildren().add(btn4);
         btn4.setLayoutX(400);
-        btn4.setLayoutY(40);
+        btn4.setLayoutY(50);
         
         EventHandler<ActionEvent> ev1 = new EventHandler<ActionEvent>() {
             public static void handle2(ActionEvent e) {
@@ -131,6 +159,12 @@ public class button extends Application {
         EventHandler<ActionEvent> ev4 = new EventHandler<ActionEvent>() {
             public static void hdnle5() {
                 Info_window();
+            }
+        };
+
+        EventHandler<ActionEvent> ev671 = new EventHandler<ActionEvent>() {
+            public static void handle671() {
+                command_window();
             }
         };
 
